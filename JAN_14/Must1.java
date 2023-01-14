83605217c86a030ddd14b1db5d94c176fd81ed2f
VaibhavGavli <114930738+VaibhavGavli@users.noreply.github.com>
2023-01-14 17:52:08 +0530

    Add files via upload

---
+++ JAN_14/Must1.java
@@ -0,0 +1,11 @@
+import java.util.StringTokenizer;
+class Must1 {
+    public static void main(String a[]) {
+       String s = "पुणे";
+  
+    	System.out.println(s.startsWith("प"));
+    	System.out.println(s.startsWith(s.length());
+    	System.out.println(s.startsWith(s.charAt(2));
+    }
+  }
+
